c0ee70e1b56c4a0f428266b552bc5ae754fef72a
ianzhang001 <24033474@qq.com>
2015-09-18 16:59:14 +0800

    up

--- worker.js
+++ worker.js
@@ -2,10 +2,13 @@ var winston = require('winston');
 
 var logger = new (winston.Logger)({
     transports: [
+      //new (winston.transports.Console)(),
       new (winston.transports.File)({ name: 'error-file', filename: 'error.log', level: 'error' }),
       //new (winston.transports.File)({ name: 'info-file', filename: 'info.log', level: 'info' })
     ]
 });
+
+var qiniu_base_url = "http://7xlw3a.com2.z0.glb.qiniucdn.com/";
 
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
@@ -43,7 +46,7 @@ MongoClient.connect(mongo_url, function(err, db) {
 
   queue.process(function(task, cb) 
   {
-  	//console.log(task); // -> { "foo": { "bar": true }, "data": [10, 20] }
+  	//logger.info(task); // -> { "foo": { "bar": true }, "data": [10, 20] }
     global_queue_cb = cb;
 
   	var image_key = task.key
@@ -55,14 +58,14 @@ MongoClient.connect(mongo_url, function(err, db) {
   		var p_id = task.p_id;
   		var c_name = task.c_name;
 
-      uploadColorImage(db, {'from_site': from_site, 'show_product_id': p_id, 'name': c_name}, from_site + "_" + p_id + "_" + c_name, cb);
+      uploadColorImage(db, {'from_site': from_site, 'show_product_id': p_id, 'name': c_name}, encodeURIComponent(from_site + "_" + p_id + "_" + c_name), cb);
 
   	} else if (type == 'i'){
   		//item
       var from_site = task.site;
       var p_id = task.p_id;
 
-      uploadItemImage(db, {'from_site': from_site, 'show_product_id': p_id}, from_site + "_" + p_id, cb);      
+      uploadItemImage(db, {'from_site': from_site, 'show_product_id': p_id}, encodeURIComponent(from_site + "_" + p_id), cb);      
 
   	}
 
@@ -79,8 +82,52 @@ var uploadColorImage = function(db, params, key, queue_cb) {
       return queue_cb(err)
     }
 
-    //console.log(doc);
-  	images = doc.images;
+  	var images = doc.images;
+
+    var success_count = 0;
+    var update_start_count = images.length;
+    var update_images = [];
+
+    for (index in images)
+    {
+      update_images[index] = {}
+      for (image_key in images[index])
+      {
+        update_images[index][image_key] = images[index][image_key]; 
+      }
+      
+      update_images[index].image = qiniu_base_url + key + "_" + index;
+    }
+    
+    var update_params = {
+      handle_image: 2,
+      images: update_images
+    }
+
+    if (doc.cover && doc.cover != "")
+    {
+      update_start_count++;
+      update_params.cover = qiniu_base_url + key + "_cover";
+      url = doc.cover
+
+      qiniuUpload(url, "shiji-goods", key + "_cover", queue_cb, function(){
+        success_count++;
+
+        if(success_count == update_start_count)
+        {
+          collection.updateOne(params, {$set: update_params}, function(err, results){
+            if (err)
+            {
+              return queue_cb(err);
+            }
+
+            return queue_cb(null);
+          });  
+        }
+        
+      });
+    }
+
 
     for (index in images)
     {
@@ -88,14 +135,20 @@ var uploadColorImage = function(db, params, key, queue_cb) {
       url = color_image.image;
 
       qiniuUpload(url, "shiji-goods", key + "_" + index, queue_cb, function(){
-        collection.updateOne(params, {$set: {"handle_image": 2}}, function(err, results){
-          if (err)
-          {
-            return queue_cb(err);
-          }
+        success_count++;
 
-          return queue_cb(null);
-        });
+        if(success_count == update_start_count)
+        {
+          collection.updateOne(params, {$set: update_params}, function(err, results){
+            if (err)
+            {
+              return queue_cb(err);
+            }
+
+            return queue_cb(null);
+          });  
+        }
+        
       });
     }
   });
@@ -114,8 +167,9 @@ var uploadItemImage = function(db, params, key, queue_cb) {
   	url = doc.cover;
 
     if(url){
+
       qiniuUpload(url, "shiji-goods", key, queue_cb, function(){
-        collection.updateOne(params, {$set: {"handle_image": 2}}, function(err, results){
+        collection.updateOne(params, {$set: {handle_image: 2, cover: qiniu_base_url + key}}, function(err, results){
           if (err)
           {
             return queue_cb(err);            
