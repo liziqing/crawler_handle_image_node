cd3b39b20b378c96077a21edecd1679bca952576
Gorden <yem_8182@qq.com>
2016-08-19 14:21:14 +0800

    up

--- behavior_report_node/app.js
+++ behavior_report_node/app.js
@@ -15,28 +15,56 @@ var logger = new (winston.Logger)({
 var configHandler = require("./confighandler");
 
 var config = configHandler.getConfig();
-
+var ops = configHandler.getops();
 var redis_client = redis.createClient(
     config['redis']['port'],
     config['redis']['host'],
     config['redis']['options']);
+var rs = Object.keys(ops['operations']);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/brand_recommend', function (req, res) {
-    if (!req.body.type) {
-        logger.error("Request body type is empty");
-        res.send("Error! Request body type is empty")
+app.post('/:r', function (req, res) {
+    var r = req.params.r;
+    var is_correct_rout = false;
+    for (var i = 0; i < rs.length; i++) {
+        if (rs[i] == r) {
+            is_correct_rout = true;
+            if (!req.body.type) {
+                logger.error("Request type is empty");
+                res.send("Error! Request type is empty")
+            }
+            else {
+                var is_correct_operations = false;
+                for (var j = 0; j < ops['operations'][r].length; j++){
+                    if (ops['operations'][r][j]['action_type'] == req.body.type) {
+                        is_correct_operations = true;
+                        var fields = ops['operations'][r][j]['fields'];
+                        var message_detail = {};
+                        for (var k = 0; k < fields.length; k++){
+                            message_detail[fields[k]] = req.body[fields[k]]
+                        }
+                        var index_name = 'logstash-' + rs[i];
+                        upload_data = {
+                            'action_type': ops['operations'][r][j]['action_type'],
+                            'message': message_detail,
+                            '@metadata': {'index_name': index_name, 'document_type': ops['operations'][r][j]['action_type']}
+                        };
+                        redis_client.rpush('logstash:list', JSON.stringify(upload_data));
+                        res.send("Success")
+                    }
+                }
+                if (is_correct_operations == false){
+                    logger.error("Operation %s is not correct", req.body.type);
+                    res.send("Error! Operation is not correct");
+                }
+            }
+        }
     }
-    else{
-        var action_type = req.body.type;
-        var brand_id = req.body.brand_id;
-        var user_id = req.body.user_id;
-        var index_name = 'logstash-brand-recommend';
-        upload_data = {'message': {'action_type': action_type, 'brand_id': brand_id, 'user_id': user_id}, '@metadata': {'index_name': index_name, 'document_type': action_type}};
-        redis_client.rpush('logstash:list', JSON.stringify(upload_data));
-        res.send("Success")
+    if (is_correct_rout == false){
+        logger.error("Route %s is not correct", r);
+        res.send("Error! Route is not correct");
     }
 });
 
